1abb9b1873eb6be37409d201151f02afc863dcc1
noeliaariza <noelia.ariza@gmail.com>
2024-03-24 11:20:21 +0100

    image testing one ok

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,24 @@
-import LinkButton from './LinkButton';
-import Preview from './Preview';
-import Form from './Form';
-import '../scss/components/Main.scss';
+import LinkButton from "./LinkButton";
+import Preview from "./Preview";
+import Form from "./Form";
+import "../scss/components/Main.scss";
 
-
-const Main = ({ infoProject, updateAvatar, setInfoProject, avatar}) => {
- 
-
+const Main = ({ infoProject, updateAvatar, setInfoProject, avatar }) => {
   return (
-    <main className='main'>
-      <section className='hero'>
-        <h2 className='title'>Proyectos molones</h2>
-        <p className='hero__text'>
+    <main className="main">
+      <section className="hero">
+        <h2 className="title">Proyectos molones</h2>
+        <p className="hero__text">
           Escaparate en línea para recoger ideas a través de la tecnología
         </p>
-        <LinkButton textContent='Ver proyectos' />
+        <LinkButton textContent="Ver proyectos" />
       </section>
 
-      <Preview infoProject={infoProject} avatar={avatar}/>
+      <Preview
+        infoProject={infoProject}
+        avatar={avatar}
+        image={infoProject.image}
+      />
 
       <Form
         setInfoProject={setInfoProject}
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,50 +1,59 @@
-import '../scss/components/Preview.scss';
-import defaultAvatar from '../images/avatar.webp';
+import "../scss/components/Preview.scss";
+import defaultAvatar from "../images/avatar.webp";
 
-function Preview({ infoProject, avatar }) {
+function Preview({ infoProject, avatar, image }) {
+  const avatarImage =
+    infoProject.image === ""
+      ? defaultAvatar
+      : avatar; /* funciona pero no sabemos porque */
 
- const avatarImage = infoProject.image === '' ? defaultAvatar : avatar /* funciona pero no sabemos porque */
-
-  
   return (
-    <section className='preview'>
-      <div className='projectImage' style = {{backgroundImage: `url 
-      ${avatarImage})`}}></div>
-      <article className='card'>
-        <h2 className='card__projectTitle'>
-          <span className='card__projectTitle--text'>
+    <section className="preview">
+      <div
+        className="projectImage"
+        style={{
+          backgroundImage: `url 
+      ${image})`,
+        }}
+      ></div>
+      <article className="card">
+        <h2 className="card__projectTitle">
+          <span className="card__projectTitle--text">
             Personal project card
           </span>
         </h2>
 
-        <div className='card__author'>
-          <div className='card__authorPhoto'>{infoProject.image}</div>
-          <p className='card__job'>{infoProject.job}</p>
-          <h3 className='card__name'>{infoProject.name}</h3>
+        <div className="card__author">
+          <div
+            className="card__authorPhoto"
+            style={{ backgroundImage: `url(${image})` }}
+          ></div>
+          <p className="card__job">{infoProject.job}</p>
+          <h3 className="card__name">{infoProject.name}</h3>
         </div>
 
-        <div className='card__project'>
-          <h3 className='card__name'>{infoProject.projectName}</h3>
-          <p className='card__slogan'>{infoProject.slogan}</p>
-          <h3 className='card__descriptionTitle'>
+        <div className="card__project">
+          <h3 className="card__name">{infoProject.projectName}</h3>
+          <p className="card__slogan">{infoProject.slogan}</p>
+          <h3 className="card__descriptionTitle">
             {infoProject.descriptionTitle}
           </h3>
-          <p className='card__description'>{infoProject.desc}</p>
+          <p className="card__description">{infoProject.desc}</p>
 
-          <div className='card__technicalInfo'>
-            <p className='card__technologies'>{infoProject.tech}</p>
+          <div className="card__technicalInfo">
+            <p className="card__technologies">{infoProject.tech}</p>
 
             <a
-              className='icon icon__www'
+              className="icon icon__www"
               href={infoProject.demo}
-              title='Haz click para ver el proyecto online'
+              title="Haz click para ver el proyecto online"
             >
               Web link
             </a>
             <a
-              className='icon icon__github'
+              className="icon icon__github"
               href={infoProject.repo}
-              title='Haz click para ver el código del proyecto'
+              title="Haz click para ver el código del proyecto"
             >
               GitHub link
             </a>
